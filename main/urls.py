10617d9302c748c82e043f6b25baf11eb5094466
Naoshi <naoshidtna97@gmail.com>
2024-08-02 21:27:16 -0400

    Add requirements.txt with project dependencies

---
+++ main/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import IndexView
+
+
+urlpatterns = [
+    path('',IndexView.as_view(), name = 'index' ),
+
+]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
